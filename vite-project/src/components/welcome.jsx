e2750d305890725ecdb9f65104c741ba38872184
GabrielDev <gabrys.wisniewski@op.pl>
2023-10-01 19:00:58 +0200

    update mobile design

--- vite-project/src/components/welcome.jsx
+++ vite-project/src/components/welcome.jsx
@@ -11,10 +11,12 @@ export default function WelcomeContainer() {
       <div className="welcome-sections-box">
         <section id="first">
           {<Title />}
+          <div className="buttons-wrapper">
           <a href="https://www.instagram.com/reel/CtmcFHSgdq0/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA==" target="_blank"><button><span>{<FaInstagram/>}</span></button></a>
           <a href="mailto: gabrys.wisniewski@op.pl"><button><span>{<BsFillEnvelopeAtFill/>}</span></button></a>
           <a href="tel:+48694638608"><button><span>{<BsFillTelephonePlusFill/>}</span></button></a>
           <a href=""><button><span>{<FaDiscord/>}</span></button></a>
+          </div>
           </section>
         <section id="second">
           <img id="icon" src={gr}/>
