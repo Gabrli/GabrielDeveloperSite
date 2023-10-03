d21c429bd571205a6a06285ba79ff3dac31f3182
GabrielDev <gabrys.wisniewski@op.pl>
2023-10-03 19:06:07 +0200

    code review: rendering projects img

--- vite-project/src/components/projects.jsx
+++ vite-project/src/components/projects.jsx
@@ -1,23 +1,21 @@
 import React from "react";
-import './styles/projects.css'
+import "./styles/projects.css";
 
-import  projectOne  from './assets/polite1.png'
-import projectTwo from './assets/pro2.png'
-import projectThree from './assets/pro3.png'
-
-const projectOneBg ={
-  backgroundImage: `url(${projectOne})`
-}
-
-const projectTwoBg = {
-  backgroundImage: `url(${projectTwo})`
-}
-
-const projectThreeBg = {
-  backgroundImage: `url(${projectThree})`
-}
+import projectOne from "./assets/polite1.png";
+import projectTwo from "./assets/pro2.png";
+import projectThree from "./assets/pro3.png";
 
 export default function Projects() {
+  const projectsImg = [
+    { backgroundImage: `url(${projectOne})` },
+    { backgroundImage: `url(${projectTwo})` },
+    { backgroundImage: `url(${projectThree})` },
+  ];
+
+  const renderImage = projectsImg.map((img, index) => {
+    return <div className="img-box" style={img} key={index}></div>;
+  });
+
   return (
     <>
       <div id="pro" className="projects-header">
@@ -34,12 +32,16 @@ export default function Projects() {
                 <b>Weather app</b>
               </p>
             </header>
-            <div style={projectTwoBg} className="img-box">
-          
-            </div>
+
+            {renderImage[1]}
+
             <div className="options-wrapper">
-              <a target="_blank" href="https://github.com/Gabrli/Weather-app"><button>Repo</button></a>
-              <a target="_blank" href="https://gabrli.github.io/Weather-app/"><button>Live</button></a>
+              <a target="_blank" href="https://github.com/Gabrli/Weather-app">
+                <button>Repo</button>
+              </a>
+              <a target="_blank" href="https://gabrli.github.io/Weather-app/">
+                <button>Live</button>
+              </a>
             </div>
           </section>
           <section className="descryption-box">
@@ -49,9 +51,11 @@ export default function Projects() {
               </p>
             </header>
             <section>
-              <p  className="project-descryption">
-              Hi there ! This project is it my new weather app, where you can check weather in your city !
-              While was creating the project I wanted practise my skils in TypeScript, CSS3 and HTML5. So this app I showed my skils in connect with Backend, Api - data bases.
+              <p className="project-descryption">
+                Hi there ! This project is it my new weather app, where you can
+                check weather in your city ! While was creating the project I
+                wanted practise my skils in TypeScript, CSS3 and HTML5. So this
+                app I showed my skils in connect with Backend, Api - data bases.
               </p>
             </section>
           </section>
@@ -63,12 +67,21 @@ export default function Projects() {
                 <b>Polite Email Builder</b>
               </p>
             </header>
-            <div style={projectOneBg} className="img-box">
-              
-            </div>
+            {renderImage[0]}
+
             <div className="options-wrapper">
-              <a target="_blank" href="https://github.com/Gabrli/PolitesEmailBuilder-new-version-2.0"><button>Repo</button></a>
-              <a target="_blank" href="https://politesemailbuilder-new-version-20.gabrys564.repl.co/"><button>Live</button></a>
+              <a
+                target="_blank"
+                href="https://github.com/Gabrli/PolitesEmailBuilder-new-version-2.0"
+              >
+                <button>Repo</button>
+              </a>
+              <a
+                target="_blank"
+                href="https://politesemailbuilder-new-version-20.gabrys564.repl.co/"
+              >
+                <button>Live</button>
+              </a>
             </div>
           </section>
           <section className="descryption-box">
@@ -78,10 +91,13 @@ export default function Projects() {
               </p>
             </header>
             <section>
-              <p  className="project-descryption">
-              Hi there! This app is it email builder which help you in write email in English.
-              While was creating the project I wanted practise my skils in the DOM Manipulation, useing vanilia JavaScript, HTML5 and CSS3.
-              So this app I showed my skils in manipulations elements on website and creating interesting and interactive web app.
+              <p className="project-descryption">
+                Hi there! This app is it email builder which help you in write
+                email in English. While was creating the project I wanted
+                practise my skils in the DOM Manipulation, useing vanilia
+                JavaScript, HTML5 and CSS3. So this app I showed my skils in
+                manipulations elements on website and creating interesting and
+                interactive web app.
               </p>
             </section>
           </section>
@@ -93,12 +109,11 @@ export default function Projects() {
                 <b>GOPR Karkonosze, sekcja kontaktowa</b>
               </p>
             </header>
-            <div style={projectThreeBg} className="img-box">
-              
-            </div>
+            {renderImage[2]}
             <div className="options-wrapper">
-            
-              <a target="_blank" href="https://gopr.org/kontakt"><button>Live</button></a>
+              <a target="_blank" href="https://gopr.org/kontakt">
+                <button>Live</button>
+              </a>
             </div>
           </section>
           <section className="descryption-box">
@@ -109,8 +124,10 @@ export default function Projects() {
             </header>
             <section>
               <p className="project-descryption">
-                Hi there ! This project is it my first serious project which was create for 
-                website GOPR -  Karkonoska Grupa Ochotnicza Pogotowia Ratunkowego - Mountain rescue.My job was to work on the contact section.
+                Hi there ! This project is it my first serious project which was
+                create for website GOPR - Karkonoska Grupa Ochotnicza Pogotowia
+                Ratunkowego - Mountain rescue.My job was to work on the contact
+                section.
               </p>
             </section>
           </section>
