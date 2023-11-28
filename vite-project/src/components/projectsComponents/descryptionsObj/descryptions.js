5c24e887f9a8b40aa661ed17530ff1701069606a
GabrielDev <gabrys.wisniewski@op.pl>
2023-11-28 16:58:12 +0100

    change: content

--- vite-project/src/components/projectsComponents/descryptionsObj/descryptions.js
+++ vite-project/src/components/projectsComponents/descryptionsObj/descryptions.js
@@ -1,13 +1,10 @@
 
 const descryptions = [
-    ' Hi there ! This project is it my new dictionary app, where you can fast researching english words and informations about him ! While was creating the project I wanted practise my skils in React +  TypeScript, CSS3 and HTML5. So in this app I showed my skils in connect with Backend, Api - data bases.',
+  " Hi there ! This project is it my new dictionary app, where you can fast researching english words and informations about him ! While was creating the project I wanted practise my skils in React +  TypeScript, CSS3 and HTML5. So in this app I showed my skils in connect with Backend, Api - data bases.",
 
+  " Hi there! This website is it my web portfolio. When createing this project, I wanted practise React and advanced CSS3. Also I thought, how improve my design skills and creative thinking.  To make a components I used a JSX technology. It is worth mentioning that I used specially written particles to create the background on the website.",
 
-    ' Hi there! This app is it email builder which help you in write email in English. While was creating the project I wanted practise my skils in the DOM Manipulation, useing vanilia JavaScript, HTML5 and CSS3. So this app I showed my skils in  manipulations elements on website and creating interesting and interactive web app.',
-
-
-    ' Hi there ! This project is it my first serious project which was create for website GOPR - Karkonoska Grupa Ochotnicza Pogotowia Ratunkowego - Mountain rescue.My job was to work on the contact section.'
-
-]
+  " Hi there ! This project is it my first serious project which was create for website GOPR - Karkonoska Grupa Ochotnicza Pogotowia Ratunkowego - Mountain rescue.My job was to work on the contact section.",
+];
 
 export { descryptions }
--- vite-project/src/components/projectsComponents/projectsList.jsx
+++ vite-project/src/components/projectsComponents/projectsList.jsx
@@ -1,13 +1,13 @@
 import { descryptions } from "./descryptionsObj/descryptions"
 import Project from "./project"
-import projectImg1 from '../../assets/polite1.png'
+import projectImg1 from '../../assets/pro1.png'
 import projectImg2 from '../../assets/pro2.png'
 import projectImg3 from '../../assets/pro3.png'
 export default function ProjectsList(){
 
     const projects = [
         {id: Math.random(), title: 'Online Dictionary', repo: 'https://github.com/Gabrli/Dictionary-app', live: 'https://gabrli.github.io/Dictionary-app/', descryption: descryptions[0], image: projectImg2},
-        {id: Math.random(), title: 'Polite Email Builder', repo: 'https://github.com/Gabrli/PolitesEmailBuilder-new-version-2.0', live: 'https://politesemailbuilder-new-version-20.gabrys564.repl.co/', descryption: descryptions[1], image: projectImg1},
+        {id: Math.random(), title: 'Web Portfolio', repo: 'https://github.com/Gabrli/GabrielDeveloperSite', live: 'https://gabrli.github.io/GabrielDeveloperSite/', descryption: descryptions[1], image: projectImg1},
         {id: Math.random(), title: 'GOPR Karkonosze, sekcja kontaktowa',  live: 'https://gopr.org/kontakt', descryption: descryptions[2], image: projectImg3}
     ]
 
